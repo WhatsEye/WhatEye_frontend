1b13b8ce2333a2e2fd817bd3b16dbfd7cbd89437
unknown <rboukhemia@gmail.com>
2025-05-23 11:58:54 +0100

    Phone responsive

--- src/lib/components/Message.svelte
+++ src/lib/components/Message.svelte
@@ -1,14 +1,13 @@
-<!--  if sender not child -->
+<!--  if msg recieved && isgroup -->
 <div class="chat-box-wrapper">
     <div>
         <div class="avatar-icon-wrapper me-1">
             <div class="avatar-icon avatar-icon-lg rounded-circle me-2">
-                <img src="https://placehold.co/150x150/EFEFEF/AAAAAA&text=Photo" alt="">
+                <img src="https://placehold.co/150x150/EFEFEF/AAAAAA&text=Photo" alt="member pic">
             </div>
         </div>
     </div>
     <div>
-        <!-- if group -->
         <div class="opacity-6 small mb-2 fw-bolder">Member name</div>
         <div class="chat-box">
             Actual msg
@@ -20,7 +19,27 @@
     </div>
 </div>
 
-<!-- if sender is child -->
+<!--  if msg received -->
+<div class="chat-box-wrapper">
+    <div>
+        <div class="avatar-icon-wrapper me-1">
+            <div class="avatar-icon avatar-icon-lg rounded-circle me-2">
+                <img src="https://placehold.co/150x150/EFEFEF/AAAAAA&text=Photo" alt="senders pic">
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="chat-box">
+            Actual msg
+        </div>
+        <small class="opacity-6">
+            <i class="lnr-calendar-full me-1"></i>
+            Time of msg
+        </small>
+    </div>
+</div>
+
+<!-- if msg sent -->
 <div class="float-end">
     <div class="chat-box-wrapper chat-box-wrapper-right">
         <div>
--- src/lib/components/RecentChat.svelte
+++ src/lib/components/RecentChat.svelte
@@ -1,26 +1,40 @@
 <script>
+    import { createEventDispatcher } from "svelte";
+    export let chatData;
     export let isActive = false;
+
+    const dispatch = createEventDispatcher();
+
+    function handleClick() {
+        dispatch('click');
+    }
 </script>
 
-<div class="row divider m-0 border"></div>
-<li class="nav-item">
-    <button type="button" class="dropdown-item {isActive ? "active" : ""}">
-        <!-- TODO: add class active for selected discussion -->
-        <div class="widget-content p-0">
-            <div class="widget-content-wrapper">
-                <div class="widget-content-left me-3">
-                    <div class="avatar-icon-wrapper">
-                        <div class="avatar-icon">
-                            <img src="https://placehold.co/150x150/EFEFEF/AAAAAA&text=Photo" alt="">
+
+<!-- svelte-ignore a11y_interactive_supports_focus -->
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<div role="button" on:click={handleClick}>
+    <div class="row divider m-0 border"></div>
+    <li class="nav-item">
+        <button type="button" class="dropdown-item" class:active={isActive}>
+            <!-- TODO: add class active for selected discussion -->
+            <div class="widget-content p-0">
+                <div class="widget-content-wrapper">
+                    <div class="widget-content-left me-3">
+                        <div class="avatar-icon-wrapper">
+                            <div class="avatar-icon">
+                                <img src={chatData.avatar || "https://placehold.co/150x150/EFEFEF/AAAAAA&text=Photo"} 
+                                    alt="{chatData.name}'s profile photo">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="widget-content-left">
-                    <div class="widget-heading">Group name / Contact name</div>
-                    <div class="widget-subheading">Last msg ?</div>
+                    <div class="widget-content-left">
+                        <div class="widget-heading">{chatData.name}</div>
+                        <div class="widget-subheading">{chatData.lastMessage}</div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </button>
-</li>
-<div class="row divider m-0 border"></div>
+        </button>
+    </li>
+    <div class="row divider m-0 border"></div>
+</div>
